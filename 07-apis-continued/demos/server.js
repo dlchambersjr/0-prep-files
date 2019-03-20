9f6a57293bc335964f7bf985e8dab5ebd83f4083
David Chambers <dlchambersjr@gmail.com>
2019-03-19 20:26:18 -0700

    adds demo

--- 07-apis-continued/demos/server.js
+++ 07-apis-continued/demos/server.js
@@ -1,26 +1,26 @@
 'use strict';
 
-// application dependencies
+// Load Environment Variables from the .env file
+require('dotenv').config();
+const superagent = require('superagent')
+
+// Application Dependencies
 const express = require('express');
 const cors = require('cors');
-const superagent = require('superagent');
 
-// configure environment variables
-require('dotenv').config();
-
+// Application Setup
+const PORT = process.env.PORT || 3000;
 const app = express();
-
-const PORT = process.env.PORT;
-
 app.use(cors());
 
-// Move the logic to the searchToLatLong function, below
-app.get('/location', searchToLatLong);
+// API Routes
+
+app.get('/location', searchToLatLong)
 
 app.get('/weather', getWeather);
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`App is up on ${PORT}`));
 
 // ERROR HANDLER
 function handleError(err, res) {
@@ -28,15 +28,12 @@ function handleError(err, res) {
   if (res) res.status(500).send('Sorry, something went wrong');
 }
 
+// Helper Functions
 
-// HELPER FUNCTIONS
-
-// Solution from day 6
+// Refactor this function
 // function searchToLatLong(query) {
 //   const geoData = require('./data/geo.json');
-//   const location = new Location(geoData);
-//   location.search_query = query;
-//   console.log(location);
+//   const location = new Location(query, geoData);
 //   return location;
 // }
 
@@ -44,56 +41,54 @@ function searchToLatLong(request, response) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
 
   return superagent.get(url)
-    .then(res => {
-      response.send( new Location(request.query.data, res));
-    })
-    .catch(error => handleError(error));
-
-}
-
-// Start with this, then refactor the .forEach to .map
-// function getWeather(request, response) {
-//   const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
-//   console.log('url', url);
-
-//   return superagent.get(url)
-//     .then(result => {
-//       const weatherSummaries = [];
-
-//       result.body.daily.data.forEach(day => {
-//         const summary = new Weather(day);
-
-//         weatherSummaries.push(summary);
-//       });
-
-//       response.send(weatherSummaries);
-//     })
-//     .catch(error => handleError(error, response));
-// }
-
-function getWeather(request, response) {
-  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
-
-  return superagent.get(url)
     .then(result => {
-      const weatherSummaries = result.body.daily.data.map(day => {
-        return new Weather(day);
-      });
-
-      response.send(weatherSummaries);
+      response.send(new Location(request.query.data, result.body.results[0]))
     })
     .catch(error => handleError(error, response));
 }
 
-function Location(query, res) {
+function Location(query, location) {
+  console.log(location.body)
   this.search_query = query;
-  this.formatted_query = res.body.results[0].formatted_address;
-  this.latitude = res.body.results[0].geometry.location.lat;
-  this.longitude = res.body.results[0].geometry.location.lng;
+  this.formatted_query = location.formatted_address;
+  this.latitude = location.geometry.location.lat;
+  this.longitude = location.geometry.location.lng;
 }
 
+function getWeather(request, response) {
+  // const darkskyData = require('./data/darksky.json');
+
+  console.log(request.query)
+
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+
+  return superagent.get(url)
+    .then(weatherResults => {
+      const weatherSummaries = weatherResults.body.daily.data.map(day => {
+        return new Weather(day);
+      });
+
+      // weatherResults.body.daily.data.forEach(day => {
+      //   let summary = new Weather(day);
+      //   weatherSummaries.push(summary);
+      // });
+      response.send(weatherSummaries);
+    })
+    .catch(error => handleError(error, response));
+
+
+  // const weatherSummaries = [];
+
+  // darkskyData.daily.data.forEach(day => {
+  //   weatherSummaries.push(new Weather(day));
+  // });
+
+  // return weatherSummaries;
+}
 
 function Weather(day) {
   this.forecast = day.summary;
   this.time = new Date(day.time * 1000).toString().slice(0, 15);
 }
+
+
