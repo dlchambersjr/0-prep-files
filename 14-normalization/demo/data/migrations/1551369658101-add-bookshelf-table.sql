e117420a8c2c974cda48b1ec1979b5297de0a133
David Chambers <dlchambersjr@gmail.com>
2019-05-18 07:33:36 -0700

    updates demo

--- 14-normalization/demo/data/migrations/1551369658101-add-bookshelf-table.sql
+++ 14-normalization/demo/data/migrations/1551369658101-add-bookshelf-table.sql
@@ -19,8 +19,19 @@ CREATE TABLE bookshelves (id SERIAL PRIMARY KEY, name VARCHAR(255));
 
 
 
+
+
+
+
+
 <!-- Query 2:  Select distinct bookshelves from the books table and insert into the bookshelves table -->
 INSERT INTO bookshelves(name) SELECT DISTINCT bookshelf FROM books;
+
+
+
+
+
+
 
 
 
@@ -60,8 +71,17 @@ ALTER TABLE books ADD COLUMN bookshelf_id INT;
 
 
 
+
+
+
+
+
 <!-- Query 4: Retrieves the primary key on each bookshelf and fills in the bookshelf id field in the books table -->
 UPDATE books SET bookshelf_id=shelf.id FROM (SELECT * FROM bookshelves) AS shelf WHERE books.bookshelf = shelf.name;
+
+
+
+
 
 
 
@@ -106,8 +126,13 @@ ALTER TABLE books DROP COLUMN bookshelf;
 
 
 
+
+
+
 <!-- Query 6: Set the bookshelf_id as the foreign key of the books table -->
 ALTER TABLE books ADD CONSTRAINT fk_bookshelves FOREIGN KEY (bookshelf_id) REFERENCES bookshelves(id);
+
+
 
 
 
